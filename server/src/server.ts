36f5cc711ae3930e4c2913ed308063ca6dbd2ba6
Jakub Kovarik <jakubkovarik@seznam.cz>
2020-07-01 21:55:11 +0200

    Various minor updates - dependencies update - create package-lock file - added dotenv file for server - added isProd check (showcase for dotenv) - lint rule for eofline

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,15 @@
+import * as dotenv from 'dotenv';
 import app from './app';
 import { Logger } from './utilities/logger';
+import { isProd } from './utilities/common.utility';
+dotenv.config();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, (): void => {
   Logger.log('Server listening on port', PORT);
+
+  if (!isProd()) {
+    Logger.log('Your app is running in dev mode');
+  }
 });
